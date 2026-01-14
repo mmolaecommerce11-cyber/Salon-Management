7cc6d28b2da778e055ec11e7906fb3629cbf0a0c
WisemanPC <mmolaecommerce11@gmail.com>
2026-01-14 12:14:30 +0200

    Added /Explore Services code in /Pages/Services/Index

--- Salon Management System/Pages/Services/Index.cshtml.cs
+++ Salon Management System/Pages/Services/Index.cshtml.cs
@@ -1,12 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Salon_Management_System.Data;
+using Salon_Management_System.Models;
 
-namespace Salon_Management_System.Pages.Services
+namespace Salon_Management_System.Pages.Services;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ApplicationDbContext _context;
+
+    public IndexModel(ApplicationDbContext context)
     {
-        public void OnGet()
+        _context = context;
+    }
+
+    public IList<SalonService> Services { get; set; } = new List<SalonService>();
+
+    [BindProperty(SupportsGet = true)]
+    public string? Search { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public string? Category { get; set; }
+
+    public async Task OnGetAsync()
+    {
+        IQueryable<SalonService> query = _context.SalonServices
+            .Where(s => s.IsActive);
+
+        if (!string.IsNullOrWhiteSpace(Search))
         {
+            query = query.Where(s => s.Name.Contains(Search));
         }
+
+        if (!string.IsNullOrWhiteSpace(Category))
+        {
+            query = query.Where(s => s.Category == Category);
+        }
+
+        Services = await query
+            .OrderBy(s => s.Category)
+            .ThenBy(s => s.Name)
+            .ToListAsync();
     }
 }
