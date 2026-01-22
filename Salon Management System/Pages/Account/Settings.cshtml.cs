39ef5014ab428a87983bf31a93118c09c7cb0cca
WisemanPC <mmolaecommerce11@gmail.com>
2026-01-22 12:47:31 +0200

    Added Account folder and added settings razor page in it. Added javascript in Layout for dark and light mode.

---
+++ Salon Management System/Pages/Account/Settings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Salon_Management_System.Pages.Account
+{
+    public class SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
